0055e549ddd28f8b68a58bb519fb77a05ea71533
Anders Schwartz <>
2022-07-30 16:12:42 -0400

    TIL-39: Load env vars through settings

--- src/main/scala/app/tilli/app/config/AppConfig.scala
+++ src/main/scala/app/tilli/app/config/AppConfig.scala
@@ -1,10 +1,51 @@
 package app.tilli.app.config
 
 import app.tilli.api.utils.HttpClientConfig
+import cats.effect.Resource
 import pureconfig.ConfigReader
 import pureconfig.generic.semiauto.deriveReader
 
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
 case class MongoDbConfig(
+  user: String,
+  password: String,
+  host: String,
+  protocol: String,
+  config: Option[String],
+  db: String,
+) {
+
+  def toMongoDbConfig: Either[Throwable, MongoDbConfigParsed] =
+    toUrl.map(url =>
+      MongoDbConfigParsed(
+        url = url,
+        db = db,
+      ))
+
+  def toUrl: Either[Throwable, String] = {
+    val chain =
+      for {
+        user <- cleanOpt(user)
+        password <- encodeOpt(password)
+        protocol <- cleanOpt(protocol)
+        config <- config.flatMap(cleanOpt)
+        host <- cleanOpt(host)
+      } yield {
+        s"$protocol://$user:$password@$host/$config"
+      }
+    chain.toRight(new IllegalArgumentException("Could not construct MongoDb config"))
+  }
+
+  def cleanOpt(s: String): Option[String] =
+    Option(s).filter(s => s != null && s.nonEmpty)
+
+  def encodeOpt(s: String): Option[String] =
+    cleanOpt(s).map(s => URLEncoder.encode(s, StandardCharsets.UTF_8))
+}
+
+case class MongoDbConfigParsed(
   url: String,
   db: String,
 )
